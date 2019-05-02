880d3d7eedab9ee93a9e732e74d17e8fafc172e7
LaiWeifeng <867777944@qq.com>
2019-05-03 01:14:06 +0800

    add comment

--- src/main/java/com/lai/quickstart/Main.java
+++ src/main/java/com/lai/quickstart/Main.java
@@ -22,6 +22,24 @@ import java.util.concurrent.ThreadFactory;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        /**
+         * Sequence class: 解释一个序号
+         *              1.通过顺序递增的需要来编号，管理进行交换的数据
+         *              2.对数据（事件）的处理过程总是沿着朱哥递增处理
+         *              3.一个Sequence 用于跟踪某个特定事件处理者（RingBuffer/Producer/Consumer）
+         *              4. 可以看做一个AtomicLong用于标识进度
+         *              5. 还有一个目的防止不同的Sequence 之间CPU 缓存伪共享（flase sharing）的问题
+         * Sequencer  class: 真正的Disruptor 的真正核心
+         *              1.实现两个接口
+         *                  1.1 SingleProducerSequencer
+         *                  1.2 MultiProducerSequencer
+         *              2.主要实现生产者和消费者之间快速，正确地传递数据的并发算法
+         *
+         * SequenceBarrier class :用于保持RingBuffer 的Main Published Sequence(Producer)和Consumer 之间的平衡关系；
+         *                        SequenceBarrier 还定义了决定Consumer是否还有可处理的事件逻辑gigit
+         */
+
+
         // 1. 实例化disruptor 对象
         OrderEventFactory orderEventFactory = new OrderEventFactory();
         int ringBufferSize = 1024 * 1024;
@@ -34,6 +52,9 @@ public class Main {
          * 3. threadFactory : 线程池
          * 4. ProducerType：消费类型
          * 5. WaitStrategy ：等待策略
+         *     BlockingWaitStrategy: 是最低效的策略，但是对CPU的消耗也是最小并且再不同部署环境中能提供更加一致性的性能表现
+         *     SleepingWaitStrategy: 的性能表现和BlockingWaitStrategy差不多，对CPU消耗也类似，但是对生产线程影响最小，适合用户异步日志类似场景
+         *     YieldingWaitStrategy：的性能是最好的，适合用户低延迟的系统，在要求极高性能且事件处理线数小于CPU逻辑核心数的场景中，推荐使用此策略；例如：CPU开启超线程的特征
          */
         Disruptor<OrderEvent> orderEventDisruptor =
                 new Disruptor<OrderEvent>(orderEventFactory, ringBufferSize, threadFactory, ProducerType.SINGLE, waitStrategy);
@@ -55,7 +76,7 @@ public class Main {
         OrderEventProducerWithTranslator orderEventProducerWithTranslator = new OrderEventProducerWithTranslator(ringBuffer);
         ByteBuffer bb = ByteBuffer.allocate(8);
         for (int i = 0; i < 100; i++) {
-            bb.putLong(0,i);
+            bb.putLong(0, i);
             orderEventProducerWithTranslator.onData(bb);
 
         }
